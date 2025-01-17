b89fbb920d885bef8cb9ee7e1bebb556a97ec287
chlloehung <144968987+chlloehung@users.noreply.github.com>
2025-01-17 01:53:32 +0000

    edit 3 files

---
+++ cylindervolume.js
@@ -0,0 +1,9 @@
+// this program *calculates* the volume of a cylinder 
+
+let diameter = 20;
+
+let height = 30;
+
+let radius = diameter/2;
+
+console.log(`${diameter} meters diameter gives a radius of ${radius} meters and a height of ${height} meters`);
---
+++ numlettergrade.js
@@ -0,0 +1,7 @@
+// this program *converts* **numeric grades** to **letter grades**
+
+let numeric = 86;
+
+let letter = "A";
+
+console.log(`${numeric} numeric is ${letter} letter`);
